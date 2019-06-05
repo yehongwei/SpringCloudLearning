922c063d39bbfddba1d1288eb765b91efc42fcff
yehon <yehongwei@chaoxing.com>
2019-06-05 09:27:08 +0800

    Ribbon/Eureka高可用

--- cloud-demo/consumer-demo/src/main/java/com/yehon/consumer/ConsumerDemoApplication.java
+++ cloud-demo/consumer-demo/src/main/java/com/yehon/consumer/ConsumerDemoApplication.java
@@ -3,6 +3,7 @@ package com.yehon.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 public class ConsumerDemoApplication {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		// 这次我们使用了OkHttp客户端,只需要注入工厂即可
 		return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
